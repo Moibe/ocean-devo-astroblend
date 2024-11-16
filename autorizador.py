f57d02c7ebcd6546c00f348e0dd8aace885b679a
Moibe <moi_estrello@hotmail.com>
2024-11-16 14:31:19 -0600

    Autorizador Deluxe

--- autorizador.py
+++ autorizador.py
@@ -2,18 +2,11 @@ import ast
 import globales
 import sulkuPypi
 
-def authenticate(username, password):
-    #Uno es el tiempo en que se tarda en obtener la cadena.
+def authenticate(username, password):    
     cadena_usuarios = sulkuPypi.getData(globales.aplicacion)    
-    #Convertir la cadena en una lista de tuplas
     lista_usuarios = ast.literal_eval(cadena_usuarios)
-    #Y otro el que se tarda en repasar la cadena.    
+    
     for u, p in lista_usuarios:
-        #Si el usuario y la contraseña son correctas...
-        if username == u and password == p:
-            print(f"{username} logged.")
-                                                
-            return True
-
-    #Si no hubo coincidencia regresas un False.    
+        if username == u and password == p:                                                           
+            return True 
     return False
